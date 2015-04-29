0da2a8e0e781153e2e7ab7d522715a1cfc336cbb
Cory Spitzer & Andrew David Burt <student@epicodus.com>
2015-04-29 10:48:34 -0700

    return and clear dealership names.

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,3 +1,16 @@
 class Dealership
 
+  attr_reader :name
+  @@dealerships = []
+
+  define_method(:initialize) do |name|
+    @name = name
+    @id = @@dealerships.length().+(1)
+    @cars = []
+  end
+
+  define_singleton_method(:clear) do
+    @@dealerships = []
+  end
+  
 end
