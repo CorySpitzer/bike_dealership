4df292d3b70750227a3a78374029394a5bbc482e
Cory Spitzer & Andrew David Burt <student@epicodus.com>
2015-04-29 14:36:30 -0700

    add and list bike dealerships via web interface.

--- app.rb
+++ app.rb
@@ -6,3 +6,18 @@ also_reload('lib/*.rb')
 get("/") do
   erb(:index)
 end
+
+get("/dealerships") do
+  @dealerships = Dealership.all()
+  erb(:dealerships)
+end
+
+get('/dealerships/new') do
+  erb(:dealership_bike_form)
+end
+
+post('/dealership_add_success') do
+  name = params.fetch('dealer-name')
+  Dealership.new(name).save()
+  erb(:success)
+end
---
+++ spec/dealership_int_spec.rb
@@ -0,0 +1,32 @@
+require('capybara/rspec')
+# require('./lib/dealership')
+# require('lib/vehicle')
+require('./app')
+Capybara.app = Sinatra::Application
+
+describe('the dealership app', {:type => :feature}) do
+
+  describe('the root path') do
+    it('displays dealership index page') do
+      visit('/')
+      expect(page).to(have_content('Bike Dealerships'))
+    end
+  end
+
+  describe('new dealership form and dealership list page') do
+    it('adds dealership from form then lists dealerships') do
+      visit('/')
+      click_link('New Dealerships')
+      expect(page).to(have_content('Add New Dealership'))
+      fill_in('dealer-name', :with => 'Velo Cult')
+      click_button('Add New Dealership')
+      click_link('Back to main page.')
+      click_link('Dealerships')
+      expect(page).to(have_content('Velo Cult'))
+    end
+  end
+
+end
+
+
+describe('')
