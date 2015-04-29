c4a1f7c34cfe8a604eefbb2590b5547ae68533fe
Cory Spitzer & Andrew David Burt <student@epicodus.com>
2015-04-29 10:05:24 -0700

    test #save, #model, and #year methods.

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -6,11 +6,38 @@ describe(Vehicle) do
     Vehicle.clear()
   end
 
+  describe("#save") do
+    it("adds a bicycle to the array of saved bicycles") do
+      test_vehicle = Vehicle.new("Specialized", "Sirrus", 2000)
+      test_vehicle.save()
+      expect(Vehicle.all()).to(eq([test_vehicle]))
+    end
+  end
+  
+
   describe('#make') do
-    it('returns the model of the vehicle') do
+    it('returns the model of the bicycle') do
       test_vehicle = Vehicle.new('Specialized', 'Sirrus', 2000)
       test_vehicle.save()
       expect(test_vehicle.make()).to(eq('Specialized'))
     end
   end
+
+  describe('#model') do
+    it("returns the year of the bicycle") do
+      test_vehicle = Vehicle.new("Specialized", "Sirrus", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.year()).to(eq(2000))
+    end
+  end
+
+  describe("#year") do
+    it("returns the year of the bicycle") do
+      test_vehicle = Vehicle.new("Specialized", "Sirrus", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.year()).to(eq(2000))
+    end
+  end
+
+
 end
